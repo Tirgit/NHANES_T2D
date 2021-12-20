ac0d47e42e3128d2dae405d236be11b3417f99f5
TheaOtteAndersen <than@sund.ku.dk>
2021-12-20 09:35:18 +0100

    EGATS risk score

--- NHANES_R/04_model_coding.R
+++ NHANES_R/04_model_coding.R
@@ -20,8 +20,25 @@ df <- readRDS("C:/Users/vrw657/Documents/GitHub/NHANES_T2D/NHANES_R/imputed_df_1
 ##### EGATS RISK SCORE #####
 ############################
 
+### create other hypertension variable in EGATS (>=140/90 mmHG or current prescription of blood pressure-lowering treatment)
 
+setwd("~/GitHub/NHANES_T2D/NHANES_R") 
+imp1 <- readRDS('imputed_df_1.rds')
 
+imp1 <- imp1 |> 
+  mutate(EGATS = ifelse(gender == "male", 2, 0)) |> 
+  mutate(EGATS = EGATS + ifelse(age >= 45 & age <=49, 1,0)) |> 
+  mutate(EGATS = EGATS + ifelse(age >= 50, 2, 0)) |> 
+  mutate(EGATS = EGATS + ifelse(BMI >= 23 & BMI <27.5, 3,0)) |> 
+  mutate(EGATS = EGATS + ifelse(BMI >= 27.5, 5, 0)) |> 
+  mutate(EGATS = EGATS + ifelse(gender == 'male' & waist <= 90, 2,0)) |> 
+  mutate(EGATS = EGATS + ifelse(gender == 'female' & waist <= 80, 2,0)) |> 
+  mutate(EGATS = EGATS + ifelse(SBP >= 140 | DBP >= 90 | now_BP_meds == 'BP meds', 2,0)) |> 
+  mutate(EGATS = EGATS + ifelse(famhist_T2D == 'family diabetes', 4,0))  
+
+  
+
+  
 ###########################
 ##### ARIC RISK SCORE #####
 ###########################
