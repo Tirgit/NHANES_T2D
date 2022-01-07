d9ec03fcfe2d63d0e24007a59a5bb24cb9a7556c
Tibor V. Varga <tirgit@hotmail.com>
2022-01-07 10:10:51 +0100

    model check, removing DPORT

--- NHANES_R/04_model_coding.R
+++ NHANES_R/04_model_coding.R
@@ -76,6 +76,11 @@ imp1 <- imp1 |>
 
 # Create the EGATS Score
 
+# correction needed: age values to be changed
+# as age can take values between 49 and 50
+# otherwise OK
+
+
 imp1 <- imp1 |> 
   mutate(EGATS = ifelse(gender == "male", 2, 0)) |> 
   mutate(EGATS = EGATS + ifelse(age >= 45 & age <= 49, 1, 0)) |> 
@@ -94,6 +99,8 @@ imp1 <- imp1 |>
 ###########################
 ##### ARIC RISK SCORE #####
 ###########################
+
+# check famhist_T2D to refer to as 'family diabetes' instead of 1
 
 ## create ARIC model:
 imp1 <- imp1 |> 
@@ -116,10 +123,13 @@ imp1 <- imp1 |>
 ##### SAN ANTONIO RISK SCORE #####
 ##################################
 
+# check famhist_T2D to refer to as 'family diabetes' instead of 1
+  
+  
 imp1 <- imp1 |> 
   mutate(Antonio = (-13.415 + 0.028 * age))|>
   mutate(Antonio = Antonio + 0.661 * (gender == "female"))|>
-  mutate(Antonio = Antonio + 0.412 * (ethnicity == "mexican"))|> ##what about the other ethnicities?
+  mutate(Antonio = Antonio + 0.412 * (ethnicity == "mexican"))|> 
   mutate(Antonio = Antonio + 0.079 * (glucose / 0.0555))|> #convert glucose into mm/dL = glucose / 0.0555 
   mutate(Antonio = Antonio + 0.018 *  SBP)|>
   mutate(Antonio = Antonio - 0.039 * (HDL / 0.0259))|> # convert HDL into mm/dL = HDL / 0.0259
@@ -130,56 +140,5 @@ imp1 <- imp1 |>
   mutate(Risk_Antonio = logit2prob(Antonio))
 
 
-############################
-##### DPoRT RISK SCORE #####
-############################
-### Predict 9-y risk ###
-imp1 <- imp1 |> 
-  mutate(DPoRT.M = 10.5971 + ifelse(hypertension_now == "hypertension", -0.2624, 0)) |> 
-  mutate(DPoRT.M = DPoRT.M + ifelse(ethnicity == "white",0 , -0.6316)) |> 
-  mutate(DPoRT.M = DPoRT.M + ifelse(heart_disease == "heart disease",-0.5355 , 0)) |> 
-  mutate(DPoRT.M = DPoRT.M + ifelse(current_smoker == "smoker",-0.1765 , 0)) |> 
-  mutate(DPoRT.M = DPoRT.M + ifelse(education %in% c("GED","some college","college"),0.2344 , 0)) |>
-  mutate(DPoRT.M = DPoRT.M + ifelse(BMI<23 & age<45,0,
-				ifelse(BMI<25 & age<45,-1.2378,
-				ifelse(BMI<30 & age<45,-1.5490,
-				ifelse(BMI<35 & age<45,-2.5437,
-				ifelse(BMI>=35 & age<45,-3.4717,
-				ifelse(BMI<23 & age>=45,-1.9794,
-				ifelse(BMI<25 & age>=45,-2.4426,
-				ifelse(BMI<30 & age>=45,-2.8488,
-				ifelse(BMI<35 & age>=45,-3.3179,-3.5857))))))))) ) |>
-  mutate(DPoRT.M = (log(365.25*9)-DPoRT.M)/0.8049 ) |> 
-  mutate(DPoRT.M = 1-exp(-exp(DPoRT.M)) )
-
-imp1 <- imp1 |> 
-  mutate(DPoRT.F = 10.5474 + ifelse(hypertension_now == "hypertension",0 ,-0.2865)) |> 
-  mutate(DPoRT.F = DPoRT.F + ifelse(ethnicity == "white",-0.4309,0)) |> 
-  mutate(DPoRT.F = DPoRT.F + ifelse(born_USA == "immigrant",0,-0.2930)) |> 
-  mutate(DPoRT.F = DPoRT.F + ifelse(education %in% c("GED","some college","college"),0.2042 , 0)) |>
-  mutate(DPoRT.F = DPoRT.F + ifelse(BMI<23 & age<45,0,
-				ifelse(BMI<25 & age<45,-0.5432,
-				ifelse(BMI<30 & age<45,-0.8453,
-				ifelse(BMI<35 & age<45,-1.4104,
-				ifelse(BMI>=35 & age<45,-2.0483,
-				ifelse(is.na(BMI)==TRUE & age<45,-1.1328,
-
-				ifelse(BMI<23 & age<65,0.0711,
-				ifelse(BMI<25 & age<65,-0.7011,
-				ifelse(BMI<30 & age<65,-1.4167,
-				ifelse(BMI<35 & age<65,-2.2150,
-				ifelse(BMI>=35 & age<65,-2.2695,
-				ifelse(is.na(BMI)==TRUE & age<65,-1.7260,
-
-
-				ifelse(BMI<23 & age>=65,-1.0823,
-				ifelse(BMI<25 & age>=65,-1.1419,
-				ifelse(BMI<30 & age>=65,-1.5999,
-				ifelse(BMI<35 & age>=65,-1.9254,
-				ifelse(BMI>=35 & age>=65,-2.1959,-1.8284))))))))))))))))) ) |>
-  mutate(DPoRT.F = (log(365.25*9)-DPoRT.F)/0.7814 ) |> 
-  mutate(DPoRT.F = 1-exp(-exp(DPoRT.F)) )
-
-imp1 <- imp1 |> 
-  mutate(DPoRT = ifelse(gender == "male",DPoRT.M,DPoRT.F)) 
-
+  
+  
