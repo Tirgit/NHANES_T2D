c3e773f98b62cf9746c09f5379f3cff230a38ab8
Alexandros Katsiferis <akatsiferis@gmail.com>
2022-01-21 17:26:40 +0100

    Update table 1 and coding
    Added height and waist in coding.
    Also added missing values for continuous variables.

--- NHANES_R/06_Table1_Creation.R
+++ NHANES_R/06_Table1_Creation.R
@@ -30,7 +30,7 @@ survey_df <- svydesign(data=cleaned_full_df_new, id=~SDMVPSU, strata=~SDMVSTRA, 
 
 k <- tableone::svyCreateTableOne(vars = c('age',  'gender', 'glucose',  'BMI', 'ethnicity', 'HDL', 
                                           'now_BP_meds', 'famhist_T2D', 'current_smoker',  'SBP',  'DBP',  
-                                          'hypertension_now', 'TG_log'),
+                                          'hypertension_now', 'TG_log', 'height', 'waist'),
                                  strata = 'survey_nr', 
                                  data = survey_df,
                                  includeNA = T,
@@ -41,3 +41,25 @@ options(max.print = 1000)
 table1 <- print(k, varLabels = T, format = "p", showAllLevels = T, catDigits = 1, 
       pDigits = 3, contDigits = 1, quote = F, missing = F, explain = F, printToggle = T, 
       noSpaces = T, cramVars = NULL, dropEqual = T)
+
+
+# We need to calculate missing values for the continuous values as well 
+# (repeat for every survey year, did it manually)
+
+cleaned_full_df_new_2017 <- cleaned_full_df_new |> filter(survey_nr == '2017_2018')
+
+survey_df_2017 <- svydesign(data=cleaned_full_df_new_2017, id=~SDMVPSU, strata=~SDMVSTRA, weights=~survey_weight, nest=TRUE)
+
+# Just for continuous variables
+
+k_2017 <- tableone::svyCreateTableOne(vars = c('age', 'glucose',  'BMI', 'HDL', 
+                                                    'SBP',  'DBP',  
+                                                    'TG_log', 'height', 'waist'),
+                                           data = survey_df_2017,
+                                           includeNA = T,
+                                           test = F, addOverall = F)
+
+
+table1_2017 <- print(k_2017, varLabels = T, format = "p", showAllLevels = T, catDigits = 1, 
+                pDigits = 3, contDigits = 1, quote = F, missing = T, explain = F, printToggle = T, 
+                noSpaces = T, cramVars = NULL, dropEqual = T)
